eefa219c9a432371c3cccde6a194489000c35b14
abelmorad <abelmorad97@gmail.com>
2023-07-23 22:41:30 +0800

    Chore

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ export default {
 ],
   theme: {
     extend: {},
+    screens: {
+      'mobile': '320px',
+      // => @media (min-width: 375px) { ... }
+
+      'tablet': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 }
