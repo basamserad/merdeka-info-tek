b3bfaa6613837fa554bc6a2febb69ebd29fc24b1
abelmorad <abelmorad97@gmail.com>
2023-07-23 21:34:55 +0800

    Feat: landing page now responsive

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+function Landing() {
+  return (
+    <section className="flex flex-col text-white items-start place-content-center hero-bg mobile:h-1/2 mobile:w-screen mobile:px-4 mobile:py-20 tablet:h-screen tablet:w-screen tablet:px-32 laptop:px-72">
+        <h1 className="capitalize text-4xl font-extrabold mb-5">
+          let's build your web application!
+        </h1>
+        <p className="mb-5">
+          We’re a full-stack JavaScript development company, creating serverless
+          cross-platform and web applications.
+        </p>
+      <Link to="/" className="bg-white text-green-700 text-lg font-medium rounded-md px-7 py-1 hover:text-white hover:bg-green-700">Let's Talk</Link>
+    </section>
+  );
+}
+
+export default Landing;
