93cdfbe77d9f0797c50f7d179b0b5fbdede7889f
abelmorad <abelmorad97@gmail.com>
2023-07-23 09:51:46 +0800

    Feat: routes established

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/Root";
+
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Landing from "./pages/Landing";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 function App() {
-  return (
-    <>
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "/",
+          element: [
+            <Landing />,
+            <About />,
+            <Services />,
+            <Portfolio />,
+            <Contact />,
+          ],
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
