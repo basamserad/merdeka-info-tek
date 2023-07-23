56544385cd8b7c7fcc07c9b8c67e193744facc71
abelmorad <abelmorad97@gmail.com>
2023-07-23 22:40:12 +0800

    Feat: about page

---
+++ src/pages/About.tsx
@@ -0,0 +1,28 @@
+function About() {
+  return (
+    <section className="flex flex-col mobile:px-4">
+      <article className="flex flex-col pt-10 gap-5">
+        <div className="flex-col">
+          <h2 className="capitalize mb-3 font-semibold">why work with us?</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
+            molestias commodi? Sapiente id vel cum rem sequi temporibus delectus
+            laboriosam asperiores eaque, commodi dolores neque enim illum,
+            blanditiis facere excepturi.
+          </p>
+        </div>
+        <div className="flex-col">
+          <h2 className="capitalize mb-3 font-semibold">We can do it for you with our special strategy</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
+            molestias commodi? Sapiente id vel cum rem sequi temporibus delectus
+            laboriosam asperiores eaque, commodi dolores neque enim illum,
+            blanditiis facere excepturi.
+          </p>
+        </div>
+      </article>
+    </section>
+  );
+}
+
+export default About;
