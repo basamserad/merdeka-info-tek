44bb75cde8e41e2f84d05969147f05ef63a4d19d
abelmorad <abelmorad97@gmail.com>
2023-07-24 15:22:46 +0800

    Feat: update removed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/Root";
-
 import About from "./pages/About";
 import Services from "./pages/Services";
-import Landing from "./pages/Landing";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import Landing from "./pages/Landing";
+import Footer from "./components/Footer";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root />,
-      children: [
-        {
-          path: "/",
-          element: [
-            <Landing />,
-            <About />,
-            <Services />,
-            <Portfolio />,
-            <Contact />,
-          ],
-        },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Header />
+      <Landing />
+      <About />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
