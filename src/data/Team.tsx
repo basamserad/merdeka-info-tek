399eaa387e9d39efef6fff9ce7cde1404c772129
abelmorad <abelmorad97@gmail.com>
2023-07-24 15:59:42 +0800

    deleted

---
+++ src/data/Team.tsx
@@ -0,0 +1,23 @@
+export default data = [
+    {
+        image: "/public/avatar/ceo.png",
+        position: "CEO",
+    },
+    {
+        image: "/public/avatar/fullstack.png",
+        position: "Fullstack Developer",
+    },
+    {
+        image: "",
+        position: "",
+    },
+    {
+        image: "",
+        position: "",
+    },
+    {
+        image: "",
+        position: "",
+    },
+
+]
