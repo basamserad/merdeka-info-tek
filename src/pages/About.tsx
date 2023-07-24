840ae7689fcb2cce313ce7dfe2971317187ed76d
abelmorad <abelmorad97@gmail.com>
2023-07-24 08:51:34 +0800

    Feat: now responsive

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,11 @@
+import TeamCard from "../components/TeamCard";
+
 function About() {
   return (
     <section className="flex flex-col mobile:px-4">
-      <article className="flex flex-col pt-10 gap-5">
+      <article className="flex flex-col pt-20 gap-5 mb-10 tablet:px-16 desktop:px-44">
         <div className="flex-col">
-          <h2 className="capitalize mb-3 font-semibold">why work with us?</h2>
+          <h2 className="capitalize mb-3 font-semibold text-4xl">about us</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
             molestias commodi? Sapiente id vel cum rem sequi temporibus delectus
@@ -12,7 +14,7 @@ function About() {
           </p>
         </div>
         <div className="flex-col">
-          <h2 className="capitalize mb-3 font-semibold">We can do it for you with our special strategy</h2>
+          <h2 className="capitalize mb-3 font-semibold text-4xl">our vision</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
             molestias commodi? Sapiente id vel cum rem sequi temporibus delectus
@@ -20,7 +22,13 @@ function About() {
             blanditiis facere excepturi.
           </p>
         </div>
-      </article>
+        </article>
+        <div className="flex-col my-20">
+          <h2 className="capitalize mb-10 font-semibold text-4xl text-center tablet:px-16 desktop:px-44">meet our team</h2>
+          <div className="flex flex-col items-center gap-5 tablet:grid tablet:grid-flow-col tablet:grid-rows-3 tablet:place-content-center laptop:grid-rows-2">
+            <TeamCard />
+          </div>
+        </div>
     </section>
   );
 }
