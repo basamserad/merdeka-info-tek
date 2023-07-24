8d27f98347dfb31386bacab5b58fbdb69fa797f9
abelmorad <abelmorad97@gmail.com>
2023-07-24 09:51:23 +0800

    Feat: services feat

---
+++ src/pages/Services.tsx
@@ -0,0 +1,14 @@
+import ServicesCard from "../components/ServicesCard"
+
+function Services() {
+  return (
+    <div className="flex flex-col px-2">
+      <h2 className="text-4xl font-semibold text-center mb-10">Products & Services</h2>
+      <div className="flex flex-col px-5 gap-5 tablet:grid tablet:grid-cols-2 laptop:px-24">
+        <ServicesCard />
+      </div>
+    </div>
+  )
+}
+
+export default Services
