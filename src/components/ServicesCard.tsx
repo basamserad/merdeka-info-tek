7d76ccba5f7b298ee816d5ca5fdcebdffabb9581
abelmorad <abelmorad97@gmail.com>
2023-07-24 15:56:24 +0800

    Style: update

--- src/components/ServicesCard.tsx
+++ src/components/ServicesCard.tsx
@@ -40,7 +40,7 @@ function ServicesCard() {
   return (
     <>
       {data.map((data) => (
-        <div className="flex flex-col items-center text-center border border-gray-300 rounded-md p-4 gap-5 tablet:flex-row">
+        <div key={data.id} className="flex flex-col items-center text-center border border-gray-300 rounded-md p-4 gap-5 tablet:flex-row">
           <img className="h-24 w-24" src={data.image} alt={data.title} />
           <div className="flex flex-col tablet:text-start">
             <p className="font-semibold mb-4 text-lg">{data.title}</p>
