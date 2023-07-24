bc0f867c7f3cb7fd25813df5cef9e87e68b3545e
abelmorad <abelmorad97@gmail.com>
2023-07-24 16:32:42 +0800

    Update

--- src/components/ServicesCard.tsx
+++ src/components/ServicesCard.tsx
@@ -23,14 +23,14 @@ function ServicesCard() {
     },
     {
       id: 4,
-      image: "https://github.com/abelmorad/merdeka-info-tek/blob/main/public/products/pwa.svg",
+      image: "https://raw.githubusercontent.com/abelmorad/merdeka-info-tek/main/public/products/pwa.svg",
       title: "Progressive Web Application",
       description:
         "PWA development has gained popularity due to its ability to deliver a seamless and engaging user experience, comparable to that of native mobile apps, without the need for users to download or install anything from an app store.",
     },
     {
       id: 5,
-      image: "https://github.com/abelmorad/merdeka-info-tek/blob/main/public/products/website.svg",
+      image: "https://raw.githubusercontent.com/abelmorad/merdeka-info-tek/main/public/products/website.svg",
       title: "Web Development",
       description:
         "Web development is a dynamic and evolving field, and developers must stay updated with the latest technologies and best practices to create high-quality and efficient web solutions that meet the needs of modern users and businesses.",
