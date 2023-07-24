a3c504bc7db76c59b0084a003fd9ddf7cdf73264
abelmorad <abelmorad97@gmail.com>
2023-07-24 15:21:28 +0800

    Style: update

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -2,7 +2,7 @@ import ServicesCard from "../components/ServicesCard"
 
 function Services() {
   return (
-    <div className="flex flex-col px-2">
+    <div id="services" className="flex flex-col px-2 pt-36">
       <h2 className="text-4xl font-semibold text-center mb-10">Products & Services</h2>
       <div className="flex flex-col px-5 gap-5 tablet:grid tablet:grid-cols-2 laptop:px-24">
         <ServicesCard />
