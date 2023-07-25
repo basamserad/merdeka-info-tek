035c5ddd84a945039208faff4063d37ec2d71143
abelmorad <abelmorad97@gmail.com>
2023-07-25 11:44:27 +0800

    Feat: portfolio finish and responsive

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,8 +1,16 @@
+import PortfolioCard from "../components/cards/PortfolioCard";
 
 function Portfolio() {
   return (
-    <div>Portfolio</div>
-  )
+    <section id="portfolio" className="flex flex-col pt-28">
+      <h2 className="text-4xl font-semibold text-center mb-10">
+        Our Portfolio
+      </h2>
+      <section className="grid grid-cols-1 gap-4 px-6 tablet:grid-cols-2 desktop:grid-cols-4 laptop:px-28">
+        <PortfolioCard />
+      </section>
+    </section>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
