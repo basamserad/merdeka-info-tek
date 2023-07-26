03ff56bd1529cad1c66cd9a47d5055f54252c8b9
abelmorad <abelmorad97@gmail.com>
2023-07-26 10:04:17 +0800

    Feat: contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,23 @@
-
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div id="contact-us" className="flex flex-col bg-black gap-8 text-white text-center items-center px-6 py-28 tablet:px-32">
+      <h2 className="capitalize mt-10 text-3xl font-semibold">
+        start your project with us
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed explicabo
+        nisi iste mollitia repudiandae laborum ipsa nemo earum cumque ducimus
+        molestias quos quia fuga maiores dignissimos dolores, modi atque
+        suscipit?
+      </p>
+      <a
+        href="#"
+        className="bg-white mb-10 text-black text-lg font-medium rounded-md px-7 py-1 laptop:px-10 laptop:py-2 hover:text-white hover:bg-black"
+      >
+        Let's talk
+      </a>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
