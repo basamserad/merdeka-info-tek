5f04293553fdb7388e08205ee44e8f8003290337
abelmorad <abelmorad97@gmail.com>
2023-08-08 06:39:11 +0800

    Removed content

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,8 @@
 
-import bismillah from "../../public/images/bismillahcalligraphy.png"
-
-
 function Landing() {
   return (
     <section className="flex flex-col text-white items-start place-content-center hero-bg h-screen w-screen mobile:px-4 mobile:py-20 mobile:text-center mobile:items-center tablet:items-start tablet:text-start tablet:px-32 laptop:px-72">
-        {/* <img src={bismillah} alt="bismillah" className="h-72 w-max place-self-center"  /> */}
-        <b className="mb-10 text-4xl laptop:text-5xl font-extrabold">Assalamualaikum Warahmatullahi Wabarakatuh!</b>
         <h1 className="capitalize text-4xl laptop:text-5xl font-extrabold mb-5">
-         
           let's build your web application!
         </h1>
         <p className="mb-5">
