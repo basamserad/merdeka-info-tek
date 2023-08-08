15720f6ca7e50bda690651b0bca3bbed8e3c254f
abelmorad <abelmorad97@gmail.com>
2023-08-08 10:21:01 +0800

    Style: change color when hover

--- src/components/NavBarDesktop.tsx
+++ src/components/NavBarDesktop.tsx
@@ -4,10 +4,11 @@ function NavBarDesktop() {
   
   return (
     <nav className="tablet:flex gap-4 font-medium mobile:hidden">
-        <a className="hover:text-yellow-500" href="#about-us" >About</a>
-        <a className="hover:text-yellow-500" href="#services">Services</a>
-        <a className="hover:text-yellow-500" href="#portfolio">Portfolio</a>
-        <a className="hover:text-yellow-500" href="#contact-us">Contact Us</a>
+        <a className="hover:text-orange-400" href="#" >Home</a>
+        <a className="hover:text-orange-400" href="#about-us" >About</a>
+        <a className="hover:text-orange-400" href="#services">Services</a>
+        <a className="hover:text-orange-400" href="#portfolio">Portfolio</a>
+        <a className="hover:text-orange-400" href="#contact-us">Contact Us</a>
     </nav>
   )
 }
